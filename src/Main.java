ce8f481d762beb08ab587eb19d81e8767563b720
KyleChutjian <kyle.chutjian@gmail.com>
2021-03-01 23:41:51 -0500

    Setup simulator

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
+        Random random = new Random();
+        int number = random.nextInt(100);
+        System.out.println("Hello, welcome to the DMV! Your number is " + number + ", please wait in line.");
+        for (int i = number + 1; i < number + 101; i++) {
+            System.out.println("Number " + i%101 + " is ready!");
+        }
+        System.out.println("Your number is ready! Unfortunately you do not have the required documents, please come back with them.");
 
     }
 
